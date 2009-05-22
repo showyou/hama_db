5b76a47214389079b17b67ac16f87ac227f6df9b
show you <showyou41@gmail.com>
2009-05-22 20:52:24 +0900

    パスがhama_dbなってたので修正

--- analyzer/quickAnalyzer.py
+++ analyzer/quickAnalyzer.py
@@ -18,8 +18,8 @@ g_mecabencode = "euc-jp"
 g_systemencode = "utf-8"
 g_outencode = g_systemencode
 _debug = False 
-homepath = "/home/yuki/public_git/hama_db/analyzer/"
-exec_path = "/home/yuki/public_git/hama_db/"
+homepath = "/home/yuki/public_git/donsuke2/analyzer/"
+exec_path = "/home/yuki/public_git/donsuke2/"
 conf_path = exec_path+"./config.json"
 
 dbSession = None
