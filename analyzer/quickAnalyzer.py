ab9ef6602d460fd5a1cb296f28c40752f81b7408
show you <showyou41@gmail.com>
2009-05-28 08:57:25 +0900

    at 修正

--- analyzer/quickAnalyzer.py
+++ analyzer/quickAnalyzer.py
@@ -182,8 +182,8 @@ def AnalyzeReply(x,session):
             d.user = x.user
             text = match2.group(2)
 
-            d.text = text
-        print "at:",
+            d.text = 'at'#text
+   	        print "at:",
 		
     # @(英数字)空白 -> user
     # この辺も直す必要ありそうだなぁ。っていうかin_reply_status_idとれるし、そっち使った方が早そう
