b41ebfe8f86ce0afe0fccb19822a2b1384888bb6
show you <showyou41@gmail.com>
2010-07-27 08:18:44 +0900

    pep2

--- analyzer/pep2junit.py
+++ analyzer/pep2junit.py
@@ -15,14 +15,14 @@ def readFile(filename):
     data = f.read()
     f.close()
     lines = data.split('\n')
-    print '<?xml version="1.0" encoding="UTF-8"?><testsuite name="nosetests" tests="1" errors="0" failures="1" skip="0"><testcase classname="analyzer.testQuickAnalyzer" name="pep8" time="0">'
+    print '<?xml version="1.0" encoding="UTF-8"?><testsuite name="nosetests" tests="1" errors="0" failures="1" skip="0">'
     for line in lines:
         #print line
         a = convert(line)
         #print a
         if len(a) > 2:
-            print "<testcase><failure type=\"exceptions.AssertionError\">",\
-                    a[3]," ".join(a[4:]),"</failure></testcase>"
+            print "<testcase classname=\"analyzer.testQuickAnalyzer\" name=\"pep8\" time=\"0\"><failure type=\"exceptions.AssertionError\">",\
+                a[3]," ".join(a[4:]),"</failure></testcase>"
     print '</testsuite>'
 
 if __name__ == "__main__":
