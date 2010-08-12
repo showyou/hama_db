3e16773d5d613b416d6e9b3d6ea3db07e7a83eae
showyou <showyou41@gmail.com>
2010-08-12 17:45:47 +0900

    応答群をファイルに移してる最中

--- common/readReplyTableFile.py
+++ common/readReplyTableFile.py
@@ -1,6 +1,16 @@
 import sys
 import simplejson
 
+def read():
+    f = open(sys.argv[1])    
+    data = f.read()
+    f.close()
+    
+    jsondata = simplejson.loads(data)
+    del jsondata["comment"]
+   
+    return jsondata
+
 
 def main():
     f = open(sys.argv[1])    
@@ -8,12 +18,13 @@ def main():
     f.close()
     
     jsondata = simplejson.loads(data)
+    del jsondata["comment"]
     for key,value in jsondata.iteritems():
-        print key, 
+        print "key",key,"value",
         for v in value:
             print v,
         print ""
-
+    return jsondata
 
 if __name__ == '__main__':
     main()
--- generator/reply.py
+++ generator/reply.py
@@ -1,6 +1,74 @@
 # -*- coding:utf-8 -*-
 import random
 
+# 下二つのInterface
+class BaseUsers():
+    def delete(self, item):
+        pass
+    
+    def __getitem__(i):
+        pass
+
+    def count():
+        pass
+
+
+# SQLAlchemy管理のユーザ
+class AlchemyUsers(BaseUsers):
+    def __init__(self, data, session):
+        self.session = session
+        self.data = data
+
+    def delete(self, item):
+        self.session.delete(item)
+
+    # a[hoge]なやつ
+    def __getitem__(self,i):
+        return self.data[i]
+
+    def count(self):
+        return self.data.count()
+
+# 配列管理のユーザ
+class ArrayUsers(BaseUsers):
+    def __init__(self, data):
+        self.data = data
+
+    def delete(self, item):
+        del self.data[item]
+
+    # a[hoge]なやつ
+    def __getitem__(i):
+        return self.data[i]
+
+    def count():
+        return len(count)
+
+
+# data = ArrayUsers or AlchemyUsers
+def pickup_same_reply(type, data):
+    sentence = ""
+    l2num = 1
+    while l2num < data.count():
+        l2 = data[l2num]
+        if l2.text == type:
+            sentence += " @"+l2.user
+            data.delete(l2)
+        else:
+            l2num += 1
+        if len(sentence) > 100: break
+
+
+# 下の奴を整理する
+def do_reply(text, table, other_replies):
+    sentence = ""
+    id = text
+    if table[text]['is_multi_reply']:
+        sentence = pickup_same_reply(text, other_replies)
+    sentence += random.choice(table[text]['reply_pattern'])   
+    return sentence
+
+
 # 返事考える
 def do(reply,session):
     sentence = ""
@@ -42,20 +110,6 @@ def do(reply,session):
             sentence = "@"+r.user+" "+s
         elif r.text == 'mayuri':
             sentence = "@"+r.user+u" トゥットゥルー♪"
-        elif r.text == 'gohan':
-            sentence = ".@"+r.user
-            l2num = 1
-            while l2num < reply.count():
-                l2 = reply[l2num]
-                if l2.text == "gohan":
-                    sentence += " @"+l2.user
-                    session.delete(l2)
-                else:
-                    l2num += 1
-                if len(sentence) > 100: break
-            s = random.choice((u'ありがとー、おいしー',u'いっただっきまーす',
-                            u'ありがと、でも今はいいやー'))
-            sentence += " "+s
         elif r.text == 'baribari':
             sentence = ".@"+r.user
             l2num = 1
